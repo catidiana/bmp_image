482608a99a41a3ee4252b63432409ba13dfd5241
catidiana <50684862+catidiana@users.noreply.github.com>
2019-06-04 18:31:33 +0300

    Update bmp.cpp

--- src/bmp.cpp
+++ src/bmp.cpp
@@ -24,27 +24,27 @@
 
 #pragma pack(push, 1)
 struct BMPHeader {
-  char magic_number[2];
-  u32  file_size;
-  u16  reserved1;
-  u16  reserved2;
-  u32  offset_bits;
+    char magic_number[2];
+    u32  file_size;
+    u16  reserved1;
+    u16  reserved2;
+    u32  offset_bits;
 };
 #pragma pack(pop)
 
 #pragma pack(push, 1)
 struct BMPImageHeader {
-  u32 header_size;
-  u32 width;
-  u32 height;
-  u16 planes;
-  u16 bit_count;
-  u32 compression;
-  u32 image_size;
-  u32 ppm_x;
-  u32 ppm_y;
-  u32 clr_used;
-  u32 clr_important;
+    u32 header_size;
+    u32 width;
+    u32 height;
+    u16 planes;
+    u16 bit_count;
+    u32 compression;
+    u32 image_size;
+    u32 ppm_x;
+    u32 ppm_y;
+    u32 clr_used;
+    u32 clr_important;
 };
 #pragma pack(pop)
 
@@ -52,37 +52,37 @@ struct BMPImageHeader {
 int
 main()
 {
-  u32 width  = 800;
-  u32 height = 500;
-  u32 pixels_count = width * height;
-  u32 image_size = pixels_count * 3;
-
-  BMPHeader file_header = {{'B', 'M'}};
-  file_header.file_size = sizeof (BMPHeader) + sizeof (BMPImageHeader) + image_size;
-
-  BMPImageHeader image_header = {};
-  image_header.header_size = 40;
-  image_header.width      = width;
-  image_header.height     = height;
-  image_header.planes     = 1;
-  image_header.bit_count  = 24;
-  image_header.image_size = file_header.file_size;  // Shoudn't this be image_size?
-  u32 dpi = 96;
-  u32 ppm = dpi * 39.375;
-  image_header.ppm_x      = ppm;
-  image_header.ppm_y      = ppm;
-
-  Pixel *pixels = (Pixel *) malloc (sizeof (Pixel) * pixels_count);
-
+    u32 width  = 800;
+    u32 height = 500;
+    u32 pixels_count = width * height;
+    u32 image_size = pixels_count * 3;
+    
+    BMPHeader file_header = {{'B', 'M'}};
+    file_header.file_size = sizeof (BMPHeader) + sizeof (BMPImageHeader) + image_size;
+    
+    BMPImageHeader image_header = {};
+    image_header.header_size = 40;
+    image_header.width      = width;
+    image_header.height     = height;
+    image_header.planes     = 1;
+    image_header.bit_count  = 24;
+    image_header.image_size = file_header.file_size;  // Shoudn't this be image_size?
+    u32 dpi = 96;
+    u32 ppm = dpi * 39.375;
+    image_header.ppm_x      = ppm;
+    image_header.ppm_y      = ppm;
+    
+    Pixel *pixels = (Pixel *) malloc (sizeof (Pixel) * pixels_count);
+    
     FILE *image = fopen ("image.bmp", "wb");
     fwrite (&file_header,  1, sizeof (BMPHeader), image);
     fwrite (&image_header, 1, sizeof (BMPImageHeader), image);
-
+    
     radial_gradient_fill (pixels, width, height, 0x800000, 0x000080);
-
+    
     fwrite (pixels, sizeof (Pixel), pixels_count, image);
     fclose(image);
     free (pixels);
-
-  return 0;
+    
+    return 0;
 }
